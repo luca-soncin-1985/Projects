8d41832d9b9651688080c4ea1495560d56ee9d86
post_lupy <lupy@gmx.com>
2023-05-01 17:38:39 +0200

    Google Weather completed

---
+++ Practice/Web_Scraping/Google weather/cloudy.py
@@ -0,0 +1,17 @@
+from requests_html import HTMLSession
+
+s = HTMLSession()
+
+query = "london"
+url = f"https://www.google.com/search?q=weather+{query}"
+
+r = s.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+                        "Accept-Language": "en-US,en;q=0.9,ar;q=0.8"})
+
+temp = r.html.find("span#wob_tm", first=True).text
+
+unit = r.html.find("div.vk_bk.wob-unit span.wob_t", first=True).text
+
+desc =r.html.find("div.VQF4g", first=True).find("span#wob_dc", first=True).text
+
+print(query,temp,unit,desc)
