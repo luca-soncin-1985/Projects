69993ef08bf6a8310ae960775e28b1ecebc5558e
post_lupy <lupy@gmx.com>
2023-05-02 10:31:24 +0200

    Weekly created

--- Practice/Web_Scraping/Google weather/cloudy.py
+++ Practice/Web_Scraping/Google weather/cloudy.py
@@ -8,9 +8,9 @@ url = f"https://www.google.com/search?q=weather+{query}"
 r = s.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
                         "Accept-Language": "en-US,en;q=0.9,ar;q=0.8"})
 
-temp = r.html.find("span#wob_tm", first=True).text
+temp = r.html.find("span#wob_tm", first=True).text #uses the # to search for the item name
 
-unit = r.html.find("div.vk_bk.wob-unit span.wob_t", first=True).text
+unit = r.html.find("div.vk_bk.wob-unit span.wob_t", first=True).text #uses the space to search for sub category
 
 desc =r.html.find("div.VQF4g", first=True).find("span#wob_dc", first=True).text
 
